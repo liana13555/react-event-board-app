14d955550a0843fd33c5d6e07b297c243d1a8efc
Liana Baryshova <barishovaliana@gmail.com>
2021-08-30 16:21:22 +0200

    Update files

---
+++ src/components/Event/Event.jsx
@@ -0,0 +1,48 @@
+import PropTypes from 'prop-types';
+import { FaMapMarkerAlt, FaUserAlt, FaCalendarAlt, FaClock } from "react-icons/fa";
+// import { formatEventStart } from 'utils/formatEventStart';
+// import {formatEventDuration} from 'utils/formatEventDuration';
+import { formatEventStart, formatEventDuration } from 'utils';
+// import { iconSize } from 'constants';
+import css from './Event.module.css';
+
+export const Event = ({ name, location, speaker, type, start, end }) => {
+    const formattedStart = formatEventStart(start);
+    const duration = formatEventDuration(start, end);
+
+    return (    
+    <div className={css.event}>
+      <h2 className={css.title}>{name}</h2>
+      <p className={css.info}>
+        <FaMapMarkerAlt className={css.icon} size={16} />   
+        {location}
+      </p>
+      <p className={css.info}>
+        <FaUserAlt className={css.icon} size={16}/>
+        {speaker}
+      </p>
+      <p className={css.info}>
+        <FaCalendarAlt className={css.icon} size={16}/>
+        {formattedStart}
+      </p>
+      <p className={css.info}>
+        <FaClock className={css.icon} size={16} />
+        {duration}
+      </p>
+            {/* <span className={css.chip}>{type}</span> */}
+      <span className={`${css.chip} ${css[type]}`}>{type}</span>
+    </div>
+  );
+}
+
+// size={iconSize.sm}
+
+Event.propTypes = {
+    name: PropTypes.string.isRequired,
+    location: PropTypes.string.isRequired,
+    speaker: PropTypes.string.isRequired,
+    type: PropTypes.string.isRequired,
+    start: PropTypes.string.isRequired,
+    end: PropTypes.string.isRequired,
+}
+
